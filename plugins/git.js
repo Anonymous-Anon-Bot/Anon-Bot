cea72e4babbcf66dd14e4f79ee779ea757acb932
Monuzz-Mkz <92070795+Monuzz-Mkz@users.noreply.github.com>
2021-10-25 03:41:21 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "http://imgur.com/a/LqEQujG.jpeg";
+  r_text[0] = "https://i.imgur.com/JDAq7br.jpeg";
     
     
     var i = Math.floor(1*Math.random())
