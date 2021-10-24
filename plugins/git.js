a0c4d21075071d399d56d360490533f6ebff40f0
Monuzz-Mkz <92070795+Monuzz-Mkz@users.noreply.github.com>
2021-10-25 02:56:13 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -19,7 +19,6 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Creater Anandh*
-
 *To check update .update   To update Bot .update now*
 
 *Bot grp* : https://chat.whatsapp.com/Ktg2pOcryOD8T7FmKjauXy
