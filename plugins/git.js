9296331a3f8be2ffb48fcbf893200c1bd76cdf62
Monuzz-Mkz <92070795+Monuzz-Mkz@users.noreply.github.com>
2021-10-25 02:20:37 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -21,13 +21,13 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*creater Amal*
 *To check update .update   To update Bot .update now*
 
-*Bot making video : https://youtu.be/fd0KPblop-k*
+*Bot making video : Coming soon😊*
 
-*Yt_PASSWORD: _________(password on this video watch it full)*
+*Bot grp* : https://chat.whatsapp.com/Ktg2pOcryOD8T7FmKjauXy
 
-*githublink : https://github.com/cyberchekuthan/Amalser_v2*
+*Pinky githublink : https://github.com/Monuzz-Mkz/Pinky*
 
-*audio commads : https://github.com/cyberchekuthan/Amalser_v2/tree/master/uploads*
+*Aswathy githublink : https://github.com/Monuzz-Mkz/aswathyy*
 `}) 
 
 }));
