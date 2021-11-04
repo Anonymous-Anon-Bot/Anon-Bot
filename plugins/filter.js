70072ff0ac3a91b848e4d45698c28ac198ba744d
Monuzz-Mkz <92070795+Monuzz-Mkz@users.noreply.github.com>
2021-11-04 13:10:54 +0530

    Update filter.js

--- plugins/filter.js
+++ plugins/filter.js
@@ -131,7 +131,7 @@ const array = ['name entha','Helo','King','Kooi','Love','Thamasha','big fan','ch
 array.map( async (a) => {
 let pattern = new RegExp(`\\b${a}\\b`, 'g');
 if(pattern.test(message.message)){
-       await message.client.sendMessage(message.jid, fs.readFileSync('./uploads/media/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
+       await message.client.sendMessage(message.jid, fs.readFileSync('./bgmone/' + a + '.mp3'), MessageType.audio, { mimetype: Mimetype.mp4Audio, quoted: message.data, ptt: true})
 }
 });
     }
